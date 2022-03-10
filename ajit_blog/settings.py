dc9cbff6bf0e51bc25d4d9d2b1157c5bf2f353a4
Ajit <ajitg131@gmail.com>
2022-03-10 11:58:59 +0530

    Updated Static root

--- ajit_blog/settings.py
+++ ajit_blog/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles') # addede for heroku deployment
 STATIC_URL = '/static/'
 
 MEDIA_ROOT =os.path.join(BASE_DIR,'media') #WILL STORE THE UPLOADED FILES
